6df8508fa6ff8879aec3083e4b4f8880a15b78c7
Naveen <none.wiseguy@gmail.com>
2015-02-20 23:04:03 -0700

    Recommit and push to heroku

--- db/migrate/20150217133000_remove_valve_from_treatment_plant.rb
+++ db/migrate/20150217133000_remove_valve_from_treatment_plant.rb
@@ -1,6 +1,6 @@
 class RemoveValveFromTreatmentPlant < ActiveRecord::Migration
   def change
-    remove_column :treatment_plants, :valve, :boolean
+    #remove_column :treatment_plants, :valve, :boolean
     rename_column :treatment_plants, :height, :volume
     add_column :treatment_plants, :capacity, :integer
   end
