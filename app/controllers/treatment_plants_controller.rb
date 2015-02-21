5c80e0cd73c633e0cb29c3e569f67dcb21be7e41
Naveen <none.wiseguy@gmail.com>
2015-02-21 00:45:57 -0700

    Updation error correction

--- app/controllers/treatment_plants_controller.rb
+++ app/controllers/treatment_plants_controller.rb
@@ -30,8 +30,7 @@ skip_before_action :verify_authenticity_token
 	  @treatment_plant.update(volume: row[1].to_i)
 	  render text: "OK \n new line #{row[0]} \n #{row[1]}"
 	 end
-    else render text: "Nothing !"
-    render text: "#{request.raw_post()}"
+    else render text: "Nothing !#{request.raw_post()}"
 	
     end
   end
