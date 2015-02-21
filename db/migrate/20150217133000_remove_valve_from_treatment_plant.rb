3f3f38b691aa6574d3b67cf64d84b5f065436963
Naveen <none.wiseguy@gmail.com>
2015-02-20 22:39:50 -0700

    Heroku app crash fix attempt

--- db/migrate/20150217133000_remove_valve_from_treatment_plant.rb
+++
@@ -1,7 +0,0 @@
-class RemoveValveFromTreatmentPlant < ActiveRecord::Migration
-  def change
-    remove_column :treatment_plants, :valve, :boolean
-    rename_column :treatment_plants, :height, :volume
-    add_column :treatment_plants, :capacity, :integer
-  end
-end
